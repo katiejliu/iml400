8d40c4253e8359122c753de2f3130d441ba7afb8
katiejliu <78124298+katiejliu@users.noreply.github.com>
2021-09-16 00:44:32 -0700

    part2
    jquery additions

---
+++ project-1/main.js
@@ -0,0 +1,31 @@
+// $(document).ready(function () {
+//     $(".screen").fadeOut();
+//     $("#keyboard").hover(
+//       function () {
+//         $(".screen").fadeOut();
+//       },
+//       function () {
+//         $(".screen").fadeIn();
+//       }
+//     );
+//   });
+  
+
+$(document).ready(function () {
+  $(".screen").fadeOut();
+    $("#yikes").hover(
+      function () {
+        $(".screen").fadeIn();
+      },
+      function () {
+        $(".screen").fadeOut();
+      }
+    );
+  });
+  
+  $(function () {
+    $(
+      "#computer1, #keyboard, #computer2"
+    ).draggable();
+
+  });
